f5c1db0d11bb54ca6d26405595e44237491a636d
Matthew Miguel <mattjm1007@gmail.com>
2026-02-17 23:29:41 -0500

    added site header / nav

--- src/data/lists.js
+++ src/data/lists.js
@@ -64,7 +64,7 @@ export const socialLinks = [
   {
     name: "Email",
     icon: email,
-    link: "mattjm1007@gmail.com",
+    link: "matthewmiguel.dev@gmail.com",
   },
   {
     name: "LinkedIn",
